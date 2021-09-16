47f87c13ade22f964569789cb587329057640176
doVa <doVaris@users.noreply.github.com>
2021-09-16 13:31:16 +0300

    Add responsive helper and style ColorBox

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js';
+import sizes from './sizes';
 
 function darker(props) {
   return chroma(props.background).luminance() >= 0.4 ? '#222' : '#fff';
@@ -19,6 +20,18 @@ export default {
     marginBottom: '-3.8px',
     '&:hover button': {
       opacity: 1,
+    },
+    [sizes.down('lg')]: {
+      width: props => (props.showingFullPalette ? '25%' : '20%'),
+      height: props => (props.showingFullPalette ? '20%' : '50%'),
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: props => (props.showingFullPalette ? '10%' : '20%'),
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: props => (props.showingFullPalette ? '5%' : '10%'),
     },
   },
   copyText: {
